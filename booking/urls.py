3e8d919519c007a81d104c05cd4c101a3946e5b9
Joel Joe <joelezra10@gmail.com>
2024-03-25 07:36:41 +0000

    add authentication

--- booking/urls.py
+++ booking/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('book/', views.booking_view, name='booking'),
+    path('booking/', views.booking_view, name='booking'),
     path('booking-success/', views.booking_success, name='booking_success'),
 ]
--- warathai/settings.py
+++ warathai/settings.py
@@ -139,6 +139,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
